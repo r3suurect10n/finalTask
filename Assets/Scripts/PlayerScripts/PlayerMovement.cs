0e33ce0aa98ea1dfbcd6502cc75d852307f1917a
r3suurect10n <sosnin.a.a@yandex.ru>
2025-03-07 15:20:55 +0300

    Added test scene

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Move")]   
+    
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _jumpPower;
+
+}
